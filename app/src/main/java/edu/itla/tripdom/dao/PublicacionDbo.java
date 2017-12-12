43216ea8dc3f93fd6c24f2c9ece80dfba78bd4ff
estarlinvelez <estarlinvelez@hotmail.com>
2017-12-12 00:22:52 -0400

    26 Noviembre

--- app/src/main/java/edu/itla/tripdom/dao/PublicacionDbo.java
+++ app/src/main/java/edu/itla/tripdom/dao/PublicacionDbo.java
@@ -12,6 +12,8 @@ import java.util.Date;
 import java.util.List;
 
 import edu.itla.tripdom.entity.Publicacion;
+import edu.itla.tripdom.entity.TipoUsuario;
+import edu.itla.tripdom.entity.Usuario;
 
 /**
  * Created by Estarlin Velez on 2/12/17.
@@ -32,7 +34,7 @@ public class PublicacionDbo {
         SQLiteDatabase db = connection.getWritableDatabase();
 
         ContentValues cv = new ContentValues();
-        cv.put("fecha", publicacion.getFecha());
+        cv.put("fecha", DF.format(publicacion.getFecha()));
         cv.put("usuario_id", publicacion.toString());
         cv.put("descripcion", publicacion.getDescripcion());
         cv.put("costo", publicacion.getCosto());
@@ -58,15 +60,15 @@ public class PublicacionDbo {
         Cursor cursor = db.query("publicacion", campos, null, null, null, null, null);
 
         cursor.moveToFirst();
-        while (!cursor.isAfterLast()){
+        while (!cursor.isAfterLast()) {
             Publicacion p = new Publicacion();
 
             p.setId(cursor.getInt(cursor.getColumnIndex("id")));
-           try{
-               p.setFecha(DF.parse(cursor.getString(cursor.getColumnIndex("fecha"))));
-           }catch (ParseException ex){
-               p.setFecha(new Date());
-           }
+            try {
+                p.setFecha(DF.parse(cursor.getString(cursor.getColumnIndex("fecha"))));
+            } catch (ParseException ex) {
+                p.setFecha(new Date());
+            }
 
             p.setUsuarioid(StringToUser(cursor.getString(cursor.getColumnIndex("usuario"))).getId());
             p.setDescripcion(cursor.getString(cursor.getColumnIndex("descripcion")));
@@ -74,6 +76,28 @@ public class PublicacionDbo {
             p.setEstado(cursor.getString(cursor.getColumnIndex("estado")));
             p.setCupo(cursor.getInt(cursor.getColumnIndex("cupo")));
             p.setUsuario(StringToUser(cursor.getString(cursor.getColumnIndex("usuario"))));
+            p.setOrigen(cursor.getString(cursor.getColumnIndex("origen")));
+            publicacion.add(p);
+            cursor.moveToNext();
 
+        }
+        cursor.close();
+        db.close();
+            return publicacion;
 
+        }
 
+            private Usuario StringToUser(String s){
+             Usuario u = new Usuario();
+            String[] valores = s.split("=");
+
+        u.setId(Integer.parseInt(valores[1].substring(0, valores[1].indexOf(", nombre"))));
+        u.setNombre(valores[2].substring(valores[2].indexOf("'")+1, valores[2].indexOf("',")));
+        u.setTipousuario(TipoUsuario.CLIENTE);
+        u.setTelefono(valores[4].substring(valores[4].indexOf("'")+1, valores[4].indexOf("',")));
+        u.setEmail(valores[5].substring(valores[5].indexOf("'")+1, valores[5].indexOf("'}")));
+
+        return u;
+
+    }
+}
